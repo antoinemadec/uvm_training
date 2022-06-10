eac7c554a2373db82d550503010ca1a10c70af72
antoinemadec <ajamadec@gmail.com>
2022-06-10 14:30:37 +0200

    fix(install): misc

--- bin/install.sh
+++ bin/install.sh
@@ -17,7 +17,7 @@ curl -O https://www.python.org/ftp/python/3.8.1/Python-3.8.1.tgz
 tar -xzf Python-3.8.1.tgz
 cd Python-3.8.1/
 ./configure --enable-optimizations
-make altinstall
+sudo make altinstall
 
 ssh-keygen
 ssh-copy-id formation6@pc-formation6

f4400ca98d20d6ee52eff18106fb604aedea0ef4
antoinemadec <ajamadec@gmail.com>
2022-06-10 12:14:26 +0200

    feat: add install scripts

---
+++ bin/install_dotfiles.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -e
+
+install_latest_neovim() {
+  local url
+  local filename
+  url=https://github.com/neovim/neovim/releases/download/stable
+  filename=nvim-linux64.tar.gz
+  mkdir -p ~/src
+  curl -sSL "${url}/${filename}" | tar -C "${HOME}/src" -xz -f -
+}
+
+git clone http://github.com/antoinemadec/dotfiles
+cat << EOF > ~/.gitconfig
+[color]
+  ui = auto
+EOF
+
+install_latest_neovim
+pip3.8 install neovim --user
+
+echo "pre_path ~/src/nvim-linux64/bin" > ~/.bashrc.local

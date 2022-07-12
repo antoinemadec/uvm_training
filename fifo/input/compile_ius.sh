2a02dc4e8fc594a2c3ad2fe805595f33a77ab867
antoinemadec <ajamadec@gmail.com>
2022-07-12 14:26:34 +0200

    feat: copy simulator files in sim/ dir
    Don't ask the user to modify the script anymore

---
+++ fifo/input/compile_ius.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+IUS_HOME=`ncroot`
+irun -vtimescale 1ns/1ps -uvmhome ${IUS_HOME}/tools/methodology/UVM/CDNS-1.2 \
++incdir+../../../verif_utils \
++incdir+../tb/fifo_in/sv \
++incdir+../tb/fifo_out/sv \
++incdir+../tb/top/sv \
++incdir+../tb/top_test/sv \
++incdir+../tb/top_tb/sv \
+-F ../dut/files.f \
+../../../verif_utils/verif_utils_pkg.sv \
+../tb/fifo_in/sv/fifo_in_pkg.sv \
+../tb/fifo_in/sv/fifo_in_if.sv \
+../tb/fifo_out/sv/fifo_out_pkg.sv \
+../tb/fifo_out/sv/fifo_out_if.sv \
+../tb/top/sv/top_pkg.sv \
+../tb/top/sv/top_cov_if.sv \
+../tb/top_test/sv/top_test_pkg.sv \
+../tb/top_tb/sv/top_th.sv \
+../tb/top_tb/sv/top_tb.sv \
++UVM_TESTNAME=top_test  $*

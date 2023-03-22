71fa69331f45a0a2084da86985079e2f94d2aa53
antoinemadec <ajamadec@gmail.com>
2023-03-22 10:10:31 +0000

    feat: add support for VCS in fifo example

--- fifo/dut/fifo.sv
+++ fifo/dut/fifo.sv
@@ -1,28 +1,18 @@
 module fifo (
   input clk,
   // data_in
-  input [31:0] data_in,
-  input        data_in_vld,
-  output       data_in_rdy,
+  input [31:0]        data_in,
+  input               data_in_vld,
+  output logic        data_in_rdy,
   // data_out
   output logic [15:0] data_out,
-  output        data_out_vld,
-  input         data_out_rdy
+  output logic        data_out_vld,
+  input               data_out_rdy
 );
 
   localparam FIFO16_SIZE = 8;
 
   bit [15:0] q16[$];
-
-  assign data_in_rdy = q16.size() <= (FIFO16_SIZE - 2);
-  assign data_out_vld = (q16.size() > 0);
-
-  always @(*) begin
-    if (q16.size() > 0)
-      data_out = q16[0];
-    else
-      data_out = 1'bX;
-  end
 
   always_ff @(posedge clk) begin
     if (data_in_vld && data_in_rdy) begin
@@ -31,6 +21,18 @@ module fifo (
     end
     if (data_out_vld && data_out_rdy) begin
       void'(q16.pop_front());
+    end
+    if (q16.size() <= (FIFO16_SIZE - 2)) begin
+      data_in_rdy = 1;
+    end else begin
+      data_in_rdy = 0;
+    end
+    if (q16.size() > 0) begin
+      data_out = q16[0];
+      data_out_vld = 1;
+    end else begin
+      data_out = 1'bX;
+      data_out_vld = 0;
     end
   end
 
